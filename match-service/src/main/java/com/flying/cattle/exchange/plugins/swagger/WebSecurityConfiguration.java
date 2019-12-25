c938be6e1453af9c4d91838e46f2054cd1707f8d
dt <dt@dt-PC>
2019-12-25 17:52:24 +0800

    修改静态资源配置

--- match-service/src/main/java/com/flying/cattle/exchange/plugins/swagger/WebSecurityConfiguration.java
+++ match-service/src/main/java/com/flying/cattle/exchange/plugins/swagger/WebSecurityConfiguration.java
@@ -1,8 +1,15 @@
 package com.flying.cattle.exchange.plugins.swagger;
 
+import java.math.BigDecimal;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 /**
  * @ClassName: DataUtil
@@ -12,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebSecurityConfiguration implements WebMvcConfigurer {
-
+	
     /**
      * swagger资源路径映射
      * @param registry
@@ -27,4 +34,18 @@ public class WebSecurityConfiguration implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
+    /**
+     * BigDecimal 转化为String
+     * @return
+     */
+    @Bean
+    public MappingJackson2HttpMessageConverter toStringConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(BigDecimal.class, ToStringSerializer.instance);
+        mapper.registerModule(simpleModule);
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
 }
